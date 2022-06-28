670c888e8eee74012a02b1712876477d2fe86d29
amitpandey <amitpande1008@gmail.com>
2022-06-28 14:08:21 +0530

    added firebase project with react

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+const Config = {
+    apiKey: "AIzaSyDyMET3q74DL04QXVJ6mV1aiGqWE8u2To4",
+    authDomain: "chatting-webapp-1e897.firebaseapp.com",
+    databaseURL: "https://chatting-webapp-1e897-default-rtdb.firebaseio.com",
+    projectId: "chatting-webapp-1e897",
+    storageBucket: "chatting-webapp-1e897.appspot.com",
+    messagingSenderId: "302087859077",
+    appId: "1:302087859077:web:a6b48c67c45643f43df2ec"
+  };
+  const app=firebase(Config) 
+  
